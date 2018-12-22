47fe1786612fc9e1a691ec118ae46e3b55baf4b7
kosmyninalex <kosmyninalex@gmail.com>
2018-12-22 13:15:32 +0400

    Some changes to GoogleResultsPage

--- src/test/java/pages/GoogleResultsPage.java
+++ src/test/java/pages/GoogleResultsPage.java
@@ -1,9 +1,16 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class GoogleResultsPage extends BasePage {
     public GoogleResultsPage(WebDriver driver) {
         super(driver);
     }
+
+    public String firstReferenceInResults (String searchVal)
+    {
+        return driver.findElement(By.xpath("//*[@class=\"r\"]/a")).getText();
+    }
+
 }
--- src/test/java/test/GoogleHomeTest.java
+++ src/test/java/test/GoogleHomeTest.java
@@ -2,6 +2,10 @@ package test;
 
 import org.testng.annotations.Test;
 import pages.GoogleHomePage;
+import pages.GoogleResultsPage;
+import org.testng.Assert;
+
+import static org.testng.Assert.assertEquals;
 
 public class GoogleHomeTest extends BaseTest {
     @Test (priority = 0)
@@ -13,6 +17,14 @@ public class GoogleHomeTest extends BaseTest {
         googleHomePage.goToGoogleHomePage();
         googleHomePage.GoogleSendTextToInput("yandex.ru");
         googleHomePage.searchGoogleForInput();
+    }
+
+    @Test (priority = 1)
+    public void checkResultingPage ()
+    {
+        GoogleResultsPage googleResultsPage = new GoogleResultsPage(driver);
+        String s = googleResultsPage.firstReferenceInResults("yandex.ru");
+        assertEquals ("yandx.ru",googleResultsPage.firstReferenceInResults("yandex.ru"));
     }
 
 }
